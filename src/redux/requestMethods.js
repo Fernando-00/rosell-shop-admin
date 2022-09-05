36ff8a13441b2c494eeaf3738f01051b63e3ba7f
Fernando-00 <fernandorosell2024@u.northwestern.edu>
2022-09-05 00:48:26 -0500

    added new link for api heroku

--- src/redux/requestMethods.js
+++ src/redux/requestMethods.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { useLocation, useNavigate } from "react-router-dom";
 
 
-const BASE_URL = "http://localhost:5000/api/";
+const BASE_URL = location.hostname === "localhost" ?  "http://localhost:5000/api/":process.env.API_URL;
 
 
 
